99373716d28c6732a9f73af011b58982b8763fba
Nathan Berton <nathan.spero.berton@gmail.com>
2017-05-26 22:41:04 -0400

    Dealing now for a new game

--- palace.py
+++ palace.py
@@ -6,16 +6,39 @@ from player import Player
 from deck import Deck
 from card import Card
 
+def new_game(players,deck):
+	for i in xrange(len(players)*3): #deal each player 3 under cards 
+		players[(i%len(players))].under.append(deck.draw())
+	#seperate for loops because of sequence of deals
+	for j in xrange(len(players*3)): # 3 top cards (handle replacement after hand deal)
+		players[(j%len(players))].top.append(deck.draw())
+	for k in xrange(len(players*6)):
+		players[(k%len(players))].hand.append(deck.draw())
 
 def main():
 	print "Welcome to PALACE"
 	numPlayers = input("Please Enter Number of AI Players: ") + 1
 	numDecks = input("Please enter the number of decks to be used:")
-	deck = Deck(1)
+	deck = Deck(numDecks)
 	# Oh look list comprehensions!
 	players = [Player() for n in range(numPlayers)]
-	
-	
+	print str(len(players)) + " Players in the game"\
 
 if __name__ == '__main__':
 	main()
+
+
+# OH LOOK SOME TESTS BATMAN!
+# written for py.test so pip install pytest
+# and yes I am writing unit tests that are entirely procastinatory in nature
+def test_new_game():
+	deck = Deck(1)
+	players = [Player() for i in range(3)]
+	new_game(players,deck)
+	valid = True
+	assert(deck.get_cards_remaining() == (52 - (12*3)))
+	for player in players:
+		assert(len(player.under) == 3)
+		assert(len(player.top) == 3)
+		assert(len(player.hand) == 6)
+	
